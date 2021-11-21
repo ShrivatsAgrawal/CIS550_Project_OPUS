feec2a22666ba0e014353ae869b82d2e760ae384
skamien <70550726+skamien@users.noreply.github.com>
2021-11-20 19:07:26 -0500

    Update routes.js

--- Scripts/NojeJSRoutes/MainJobSearch/server/routes.js
+++ Scripts/NojeJSRoutes/MainJobSearch/server/routes.js
@@ -61,6 +61,67 @@ async function all_jobs(req,res) {
   
 }
 
+async function company_sentiment(req, res) {
+    var company = req.query.symbol;
+    connection.query(`WITH T1 AS (SELECT DISTINCT peerID as ID
+        FROM Peers
+        WHERE symbol = '%${company}%' or peerID = '%${company}%'),
+        T2 AS (SELECT C.companyName, C.symbol, T1.ID
+        FROM CompanyInformation C JOIN T1 ON C.symbol = T1.ID),
+        T3 AS (SELECT S.sentiment, T1.ID
+        FROM CompanySentiments S JOIN T1 ON S.symbol = T1.ID),
+        T4 AS (SELECT 'Average of peers' AS companyName, 'AVG' AS symbol, AVG(S.sentiment) as sentiment
+        FROM CompanySentiments S JOIN T1 ON S.symbol = T1.ID
+        WHERE T1.ID != '%${company}%')
+        SELECT * FROM T4
+        UNION ALL
+        SELECT T2.companyName, T2.symbol, T3.sentiment
+        FROM T2 JOIN T3 ON T2.ID = T3.ID;`, function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            res.json({ error: error })
+        } else if (results) {
+            out.push(results)
+        }
+    });
+
+    res.json({results: out});
+}
+
+async function company_jobs(req, res) {
+    var company = req.query.symbol;
+    connection.query(`WITH T1 AS (SELECT DISTINCT peerID
+        FROM Peers
+        WHERE symbol = '%${company}%' OR peerID = '%${company}%')
+        SELECT *
+        FROM IndeedJobs IJ JOIN T1 ON IJ.companySymbol = T1.peerID
+        ORDER BY postingDate DESC;`, function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            res.json({ error: error })
+        } else if (results) {
+            res.json({results: results})
+        }
+    });
+}
+
+async function company_news(req, res) {
+    var company = req.query.symbol;
+    connection.query(`WITH T1 AS (SELECT DISTINCT peerID
+        FROM Peers
+        WHERE symbol = '%${company}%' or peerID = '%${company}%)
+        SELECT *
+        FROM CompanyNews CN JOIN T1 ON CN.symbol = T1.peerID
+        ORDER BY publishedDate DESC;`, function (error, results, fields) {
+        if (error) {
+            console.log(error)
+            res.json({ error: error })
+        } else if (results) {
+             res.json({results: results})
+        }
+    });
+}
+
 
 // ********************************************
 //            SIMPLE ROUTE EXAMPLE
@@ -233,6 +294,9 @@ return res.json({error:"Please specify the ID of the match as a query parameter.
 
 module.exports = {
     all_jobs,
+    company_sentiment,
+    company_jobs,
+    company_news,
     hello,
     jersey
 }
