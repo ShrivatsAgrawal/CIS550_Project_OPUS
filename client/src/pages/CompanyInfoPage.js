e1dfaeb0ac24e3e596401658775f50c20c6ad3bd
Abhinav Atrishi <47416119+AbhinavAtrishi@users.noreply.github.com>
2021-12-03 02:43:48 -0500

    Designed Company Info Page

--- client/src/pages/CompanyInfoPage.js
+++ client/src/pages/CompanyInfoPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from "shards-react";
 
 import {
-    Image, Descriptions, Badge
+    Image, Descriptions, Badge, Progress
 } from 'antd'
 
 import { withRouter } from "react-router";
@@ -46,35 +46,46 @@ class CompanyInfoPage extends React.Component {
                 <Descriptions.Item label="Sector">{this.state.companyInfo.sector}</Descriptions.Item>
                 <Descriptions.Item label="Industry">{this.state.companyInfo.industry}</Descriptions.Item>
                 <Descriptions.Item label="Symbol">{this.state.companyInfo.symbol}</Descriptions.Item>
-                <Descriptions.Item label="Exchange">{this.state.companyInfo.exchange}</Descriptions.Item>
-                <Descriptions.Item label="Stock Price">{this.state.companyInfo.price}</Descriptions.Item>
-                <Descriptions.Item label="Currency">{this.state.companyInfo.currency}</Descriptions.Item>
-                
-
                 <Descriptions.Item label="Website" span={2}>
                     <a href={this.state.companyInfo.website} target="_blank" rel="noopener noreferrer">{this.state.companyInfo.website}</a>
                 </Descriptions.Item>
                 <Descriptions.Item label="Current Status">
                 <Badge status="processing" text="Active" />
                 </Descriptions.Item>
-                <Descriptions.Item label="Negotiated Amount">$80.00</Descriptions.Item>
-                <Descriptions.Item label="Discount">$20.00</Descriptions.Item>
-                <Descriptions.Item label="Official Receipts">$60.00</Descriptions.Item>
-                <Descriptions.Item label="Config Info">
-                Data disk type: MongoDB
-                <br />
-                Database version: 3.4
-                <br />
-                Package: dds.mongo.mid
-                <br />
-                Storage space: 10 GB
-                <br />
-                Replication factor: 3
-                <br />
-                Region: East China 1<br />
-                </Descriptions.Item>
+                <Descriptions.Item label="CEO">{this.state.companyInfo.ceo}</Descriptions.Item>
+                <Descriptions.Item label="Full Time Employees">{this.state.companyInfo.fullTimeEmployees}</Descriptions.Item>
+                <Descriptions.Item label="Company Size"><Progress
+                    strokeColor={{
+                        '0%': '#87d068',
+                        '100%': '#108ee9',
+                    }}
+                    percent={75}
+                    showInfo={false}
+                /></Descriptions.Item>
+                
             </Descriptions>
-    
+            <br/>
+            <Descriptions title="Stock Information" bordered>
+                <Descriptions.Item label="Exchange">{this.state.companyInfo.exchange}</Descriptions.Item>
+                <Descriptions.Item label="Stock Price">{this.state.companyInfo.price}</Descriptions.Item>
+                <Descriptions.Item label="Currency">{this.state.companyInfo.currency}</Descriptions.Item>
+                <Descriptions.Item label="Exchange Short">{this.state.companyInfo.exchangeShortName}</Descriptions.Item>
+                <Descriptions.Item label="Market Cap">{this.state.companyInfo.mktCap}</Descriptions.Item>
+                <Descriptions.Item label="Volume Average">{this.state.companyInfo.volAvg}</Descriptions.Item>
+            </Descriptions>    
+            <br/>
+            <Descriptions title="Contact Information" bordered>
+            <Descriptions.Item label="Address" span={2}>{this.state.companyInfo.address}</Descriptions.Item>
+            <Descriptions.Item label="City">{this.state.companyInfo.city}</Descriptions.Item>
+            <Descriptions.Item label="State">{this.state.companyInfo.state}</Descriptions.Item>
+            <Descriptions.Item label="Zip Code">{this.state.companyInfo.zip}</Descriptions.Item>
+            
+            <Descriptions.Item label="Phone Number">{this.state.companyInfo.phone}</Descriptions.Item>
+            </Descriptions>
+            <br/>
+            <Descriptions title="Official Description of the Company">
+                <Descriptions.Item>{this.state.companyInfo.description}</Descriptions.Item>
+            </Descriptions>         
             </div>
         )
     }
