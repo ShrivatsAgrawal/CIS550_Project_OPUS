3c0f756fff5daa183779251c14bd28b5aed5d42f
bushrakidwai <bkidwai@seas.upenn.edu>
2021-12-05 17:08:13 -0500

    fixing the merge conflict

--- client/src/index.js
+++ client/src/index.js
@@ -14,11 +14,8 @@ import "shards-ui/dist/css/shards.min.css"
 import CompanyJobsPage from './pages/CompanyJobsPage';
 import CompanyInfoPage from './pages/CompanyInfoPage';
 import CompanySentimentPage from './pages/CompanySentimentPage';
-<<<<<<< HEAD
 import CompanyNewsPage from './pages/CompanyNewsPage';
-=======
 import SearchJobsPage from './pages/SearchJobs'
->>>>>>> 12c92a0a2eca6999477d944c5dddb134b6591e41
 
 ReactDOM.render(
   <div>
@@ -48,16 +45,11 @@ ReactDOM.render(
 							render={() => (
 								<CompanySentimentPage />
 							)}/>
-<<<<<<< HEAD
 		<Route exact
 							path="/company/news/:symbol"
 							render={() => (
 								<CompanyNewsPage />
 							)}/>
-=======
-
-		
->>>>>>> 12c92a0a2eca6999477d944c5dddb134b6591e41
       </Switch>
     </Router>
   </div>,
