bcb3d6653948e475f28ed67ff7f87210458087d1
skamien <70550726+skamien@users.noreply.github.com>
2021-12-08 02:34:28 -0500

    added a menubar

--- client/src/pages/CompanySentimentPage.js
+++ client/src/pages/CompanySentimentPage.js
@@ -6,6 +6,13 @@ import {
   Divider,
   Progress
 } from 'antd'
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+  } from "shards-react";
 
 import MenuBar from '../components/MenuBar';
 import { getCompanySentiment } from '../fetcher'
@@ -64,7 +71,7 @@ class CompanySentimentPage extends React.Component {
 
   componentDidMount() {
     getCompanySentiment(this.state.symbol).then(res => {
-      console.log(res);
+      if (res === "no company") {this.setState({symbol: null})}
       this.setState({companyName: res.results[0].companyName})
       this.setState({ companySentimentResults: res.results[0].sentiment})
       this.setState({companyIndex: res.results[0].absoluteIndex})
@@ -79,8 +86,39 @@ class CompanySentimentPage extends React.Component {
 
     return (
       <div>
-        
+       //menubar starts here 
        <div style={{ width: '70vw', margin: '0 auto', marginTop: '5vh' }}>
+          <Navbar type="dark" theme="primary" expand="md">
+            <NavbarBrand href="/">CIS 550 OPUS</NavbarBrand>
+            <Nav navbar>
+              <NavItem>
+                <NavLink active href="/">
+                  Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink active href={"/company/info/"+this.state.symbol}>
+                  Info
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink active href={"/company/news/"+this.state.symbol}>
+                  News
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink active href={"/company/sentiment/"+this.state.symbol}>
+                  Sentiment
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink active  href={"/company/jobs/" +this.state.symbol}>
+                  Jobs
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
+          //menubar ends here
           <Divider>Company Sentiment for {this.state.companyName}</Divider>
           <p>Sentiment: what people think of this company<Progress percent={Math.trunc(this.state.companySentimentResults * 100)}/></p>
           <p>Popularity: how much people talk about this company<Progress percent={Math.trunc(this.state.companyIndex * 100)}/></p>
