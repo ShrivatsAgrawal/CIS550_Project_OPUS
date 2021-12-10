f08dcfa8b435d28f8ab69b0f62b023927f30562c
skamien <70550726+skamien@users.noreply.github.com>
2021-12-09 23:12:29 -0500

    Update MenuBar.js

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -7,41 +7,46 @@ import {
     NavLink
   } from "shards-react";
 
-class MenuBar extends React.Component {
-    render(symbol) {
-        return(
-            <Navbar type="dark" theme="primary" expand="md">
-            <NavbarBrand href="/">CIS 550 OPUS</NavbarBrand>
-            <Nav navbar>
-              <NavItem>
-                <NavLink active href="/">
-                  Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink active href={"/company/info/"+symbol}>
-                  Info
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink active href={"/company/news/"+symbol}>
-                  News
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink active href={"/company/sentiment/"+symbol}>
-                  Sentiment
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink active  href={"/company/jobs/" +symbol}>
-                  Jobs
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Navbar>
-        )
-    }
+//to use: <MenuBar symbol={[symbol]}/>
+
+function MenuBar(props){
+    return(
+        <Navbar type="dark" theme="primary" expand="md">
+        <NavbarBrand href="/">CIS 550 OPUS</NavbarBrand>
+        <Nav navbar>
+          <NavItem>
+            <NavLink active href="/">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active href={"/company/info/"+props.symbol}>
+              Info
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active href={"/company/news/"+props.symbol}>
+              News
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active href={"/company/sentiment/"+props.symbol}>
+              Sentiment
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active  href={"/company/jobs/" +props.symbol}>
+              Jobs
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active  href={"/search/jobs/" +props.symbol}>
+              Job Search
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
 }
 
 export default MenuBar
